5d29862511f7c1e65248e62d2151d7250730bc9d
szty1 <gerebtamas@gmail.com>
2023-03-13 20:55:12 +0100

    code cleanup

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
-let inputValue = "0"; // stores the value currently being input
+let inputValue = 0; // stores the value currently being input
 let storedValue; // store the previously entered input or the last result
 let operation; // stores the the selected operation
 let resetInput = false; // store if the last input was an operate or calculate button
+let fraction = false; // store if decimal point has been clicked
+
+// math functions
 
 function add(a ,b) {
     return Number(a) + Number(b);
@@ -16,80 +19,43 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    // return null if division by 0, will trigger error mesg on display
+    // return null if division by 0, will trigger error msg on display
     if (b == 0) return;
     return Number(a) / Number(b);
 }
 
-function toggleNegative() {
-    inputValue = (inputValue * -1).toString();
+// input handler functions
+
+function plusMinusButtonClicked() {
+    inputValue = (inputValue * -1);
     updateDisplay(inputValue);
 }
 
-function handleClear() {
-    // everything is reset to default (display is not updated here!)
-    inputValue = "0";
-    storedValue= undefined;
-    operation = undefined;
-    resetInput = false;
-    operateButtons.forEach(key => key.classList.remove('selected'));
-    updateDisplay(inputValue);
+function clearButtonClicked() {
+    resetCalculator(0);
 }
 
-function handleBackspace() {
+function backspaceButtonClicked() {
     // check if negative and remove the last numeric digit (incl. decimal point)
-    let firstdigit = (inputValue < 0) ? 2 : 1
-    if (inputValue.length > firstdigit) {
-        inputValue = inputValue.slice(0, -1);
+    if (!inputValue) return;
+    let strInputValue = `${inputValue}`;
+    let firstDigit = (inputValue < 0) ? 2 : 1;
+    if (strInputValue.length > firstDigit) {
+        strInputValue = strInputValue.slice(0, -1);
     } else {
-        inputValue = "0";
+        strInputValue = 0;
     }
+    inputValue = Number(strInputValue);
     updateDisplay(inputValue);
 }
 
-function updateDisplay(text) {
-    // if number is too large to display or a function returned an error show error msm on display
-    if ((Math.abs(Number(text)) > 99999999999) || text === null) {
-        text = "ERROR!";
-        handleClear();
-    }
-
-    // make sure variable is the correctg type
-    if (typeof(text) === "number") text = text.toString();
-
-    // handle negative sign (has its own container to avoid string length issues)
-    if (text < 0) {
-        text = text.substring(1);
-        negative.innerText = "-";
-    }
-    else {
-        negative.innerText = "";
-    }
-
-    // do not show decimal point if decimal digits ha no place left
-    if (text.length > 11 && text.includes(".")) {
-        text = text.substring(0, 11);
-    }
-
-    display.innerText = text;
-}
-
-function calculate() {
-    // check if operation is defined, perform calculation and display result
-    if (!operation) return;
-    let result = window[operation](storedValue, inputValue);
-    updateDisplay(result);
-
-    // if no error, move result to storedvalue, flag that an operation has been performed
-    if (result != undefined) storedValue = result;
-    resetInput = true;
-}
-
-function operateButtonClicked(btn) {
-    // store operation and display it on the button
-    operation = btn.getAttribute('data-key');
-    operateButtons.forEach(key => key.classList.remove('selected'));
-    btn.classList.add('selected');
+function operateButtonClicked(key) {
+    // find the clicked buton
+    let btn = operateButtons.find (btn => btn.getAttribute('data-key') === key);
+    
+    // set operation
+    operation = btn.getAttribute('data-operation');
+    setOperatorButtonHighlight(btn);
     
     // if another operate button was press before, don't do anything else
     if (resetInput) return;
@@ -101,7 +67,7 @@ function operateButtonClicked(btn) {
         calculate();
     } else {
         storedValue = inputValue;
-        inputValue = "0";
+        inputValue = 0;
     }
     
 }
@@ -116,28 +82,87 @@ function calculateButtonClicked() {
 function numButtonClicked(key) {
     //if a result is displayed, a new numeric input should reset the inputvalue
     if (resetInput) {
-        inputValue = "0";
+        inputValue = 0;
         resetInput = false;
     }
 
+    // prevent numbers that don't fit on screen
+    if (inputValue > 99999999999999) return;
+
+    if (fraction) {
+        inputValue = Number(`${inputValue}.${key}`);
+        fraction = false;
+    }
+    else {
+        inputValue = Number(`${inputValue}${key}`);
+    }
+    
+    updateDisplay(inputValue);
+
+    setOperatorButtonHighlight(null);
+}
+
+function decimalPointButtonClicked() {
     // prevent more than one decimal point in input
-    if (key === "." && inputValue.includes(".")) return;
+    if (fraction || !Number.isInteger(inputValue)) return;
 
-    if (inputValue === "0") inputValue = "";
+    fraction = true;
+}
 
-    // do not add number if max length is reached
-    let maxlength = (inputValue < 0) ? 12 : 11;
-    if (inputValue.length < maxlength) {
-        inputValue += key;
-        updateDisplay(inputValue);
+//
+
+function setOperatorButtonHighlight(btn) {
+    // if null, remove all highlight
+    operateButtons.forEach(key => key.classList.remove('selected'));
+    if (btn) btn.classList.add('selected');
+}
+
+function updateDisplay(num) {
+    let displayText = `${num}`;
+    if (fraction) displayText += ".";
+    
+    // if number is too large to display or a function returned an error show error msm on display
+    if ((Math.abs(num) > 99999999999999) || num === null) {
+        display.innerText = 'ERROR!';
+        return;
     }
 
-    // remove operate button highlights
-    operateButtons.forEach(key => key.classList.remove('selected'));
+    // do not show decimal point if decimal digits ha no place left
+    if (displayText.length > 12 && displayText.includes(".")) {
+        displayText = displayText.substring(0, 13);
+    }
+
+    display.innerText = displayText;
 }
+
+function calculate() {
+    // check if operation is defined, perform calculation and display result
+    if (!operation) return;
+    let result = window[operation](storedValue, inputValue);
+
+    // if no error, move result to storedvalue, flag that an operation has been performed
+    if (result != undefined) storedValue = result;
+    resetInput = true;
+
+    updateDisplay(result);
+}
+
+function resetCalculator(displayText) {
+    // everything is reset to default (display is not updated here!)
+    inputValue = 0;
+    storedValue= undefined;
+    operation = undefined;
+    resetInput = false;
+    setOperatorButtonHighlight(null);
+    updateDisplay(inputValue);
+}
+
+// input handlers
 
 function handleInput(e) {
     let key = (e.key === undefined) ? this.dataset.key : e.key;
+
+    console.log(key);
 
     switch(key) {
         case '0':
@@ -150,58 +175,55 @@ function handleInput(e) {
         case '7': 
         case '8':
         case '9':
-        case '.': 
             numButtonClicked(key);
             break;
 
+        case '.': 
+            decimalPointButtonClicked(key);
+            break;
+
         case '+':
-        case 'add':
-            operateButtonClicked(document.querySelector('[data-key="add"]'));
-            break;
-
         case '-':
-        case 'subtract':
-            operateButtonClicked(document.querySelector('[data-key="subtract"]'));
-            break;
-
         case '*':
-        case 'multiply':
-            operateButtonClicked(document.querySelector('[data-key="multiply"]'));
-            break;
-
         case '/':
-        case 'divide':
-            operateButtonClicked(document.querySelector('[data-key="divide"]'));
+            operateButtonClicked(key);
             break;
 
-        case 'Enter':
         case 'calculate':
+        case 'Enter':
             calculateButtonClicked();
             break;
 
         case 'plusminus':
-            toggleNegative();
+            plusMinusButtonClicked();
             break;
 
         case 'clear':
         case 'Escape':
-            handleClear();
+            clearButtonClicked();
             break;
 
-        case 'bckspc':
+        case 'backspace':
         case 'Backspace':
-            handleBackspace();
+            backspaceButtonClicked();
             break;
     }
 
+    // reset focus to display to prevent enter key call the focused button
+    display.focus();
+
 }
+
+const allButtons = Array.from(document.querySelectorAll('.key'));
+allButtons.forEach(key => key.addEventListener('click', handleInput));
+
+window.addEventListener('keydown', handleInput);
+
+// 
 
 const display = document.querySelector('.displaytext');
 const negative = document.querySelector('.displaynegative');
 
 const operateButtons = Array.from(document.querySelectorAll('.key.operate'));
 
-const allButtons = Array.from(document.querySelectorAll('.key'));
-allButtons.forEach(key => key.addEventListener('click', handleInput));
-
-window.addEventListener('keydown', handleInput);
+display.focus();
